2cf38fa8e5340fd853cad917b3179227826b5bdc
Talal <muhammaadtalal@gmail.com>
2022-06-20 01:41:30 +0100

    Minor fix in get_account function

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -15,13 +15,13 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 def get_account(index=None, id=None):
     if index:
         return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
     if id:
         return accounts.load(id)
-    if (
-        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
-    ):
-        return accounts[0]
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
 
 
 contract_to_mock = {
